e33cf74666084e14469b6eeb49f8f49f0a136339
Madison Vega <madison.r.vega@gmail.com>
2021-05-20 19:23:55 -0500

    added model for exercises

---
+++ models/exerciseList.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    day: {
+        type: Date,
+        required: true
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            weight: {
+                type: Number,
+                required: true
+            },
+            reps: {
+                type: Number,
+                required: true
+            },
+            sets: {
+                type: Number,
+                required: true
+            }
+
+            
+            
+        }
+    ] 
+});
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
